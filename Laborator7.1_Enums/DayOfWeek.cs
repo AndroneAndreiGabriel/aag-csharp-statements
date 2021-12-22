2841aaa9b32aff69f698f61fff6cbf5570dd992d
Andrei Androne <andrei.androne@outlook.com>
2021-12-22 21:15:21 +0200

    Laborator 7

---
+++ Laborator7.1_Enums/DayOfWeek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EnumExercices
+{
+    public enum DayOfWeek
+    {
+        Monday, //0
+        Tuesday, //1
+        Wednesday, //2 
+        Thurdsay, //3 
+        Friday, //4
+        Saturday, //5 
+        Sunday //6
+    }
+}
---
+++ Laborator7.1_Enums/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Laborator7._1_Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*DayOfWeek monday = DayOfWeek.Monday;
+            Console.WriteLine((int)monday);
+            Console.WriteLine(monday);*/
+
+            string dayOfWeek = "Mondays";
+            //bool isConvertible = Enum.TryParse(typeof(DayOfWeek), dayOfWeek, out object parseResult);
+
+            bool isConvertible = Enum.TryParse(dayOfWeek, out DayOfWeek parsedResult);
+
+            if (isConvertible)
+            {
+                DayOfWeek value = (DayOfWeek)parsedResult;
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine($"'{dayOfWeek}' cannot be converted to DayOfWeek");
+            }
+        }
+    }
+}
