e78add5f64cabb63cf8ef0bbccaf8bfcd3376c8a
Andrei Androne <andrei.androne@outlook.com>
2021-12-22 21:34:10 +0200

    Exercitiu array

---
+++ Laborator7_arrays/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Laborator7_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //citire array
+            int[] array = ReadArray("Array");
+                        
+            //tiparire
+            PrintArray("Array", array);
+        }
+
+        static int ReadNumber(string label, int defaultValue)
+        {
+            Console.Write(label);
+            string value = Console.ReadLine();
+            //Convert.ToInt32();
+            if (int.TryParse(value, out int result))
+            {
+                return result;
+            }
+
+            return defaultValue;
+        }
+
+        static int[] ReadArray(string label)
+        {
+            //citire array
+            Console.WriteLine(label);
+            int nr = ReadNumber("Elements count= ", 0);
+            int[] array = new int[nr];
+            for (int i = 0; i < array.Length; i++)
+            {
+                int element = ReadNumber($"Element[{i}]= ", 0);
+                array[i] = element;
+            }
+
+            return array;
+        }
+
+        static void PrintArray(string label, int[] array)
+        {
+            Console.WriteLine();
+            string elementList = string.Join(", ", array);
+            Console.Write($"{label}={elementList}");
+        }
+    }
+}
