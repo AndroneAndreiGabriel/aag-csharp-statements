423239fa1cd7f949bd0b98b132d1a3195a763b75
Andrei Androne <andrei.androne@outlook.com>
2022-01-26 21:11:11 +0200

    Lab10OOP

---
+++ OopAccessLevels/OopAccessLevels.Consumer/ConvertibleLaptop.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OopAccessLevels.Library;
+
+namespace OopAccessLevels.Consumer
+{
+    class ConvertibleLaptop : Laptop
+    {
+        public void DetachScreen()
+        {
+            // can be accesed because we're not in the same assembly, but it is protected internal
+            PlugIn();
+            processorFrequency = 1000;
+            Console.WriteLine("Working with the screen as a tablet");
+        }
+
+        public void AttachScreen()
+        {
+            processorFrequency = 1700;
+            Console.WriteLine("Working again like a laptop");
+        }
+    }
+}
---
+++ OopAccessLevels/OopAccessLevels.Library/Laptop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopAccessLevels.Library
+{
+    public class Laptop
+    {
+        private string processorName = "Intel i7";
+        protected int processorFrequency = 1700;
+        public void PrintSpecifications()
+        {
+            Console.WriteLine("Specs:");
+            Console.WriteLine($"   - Processor: {processorName}");
+        }
+
+        protected internal void PlugIn()
+        {
+            Console.WriteLine("Plugging in the laptop");
+        }
+    }
+}
