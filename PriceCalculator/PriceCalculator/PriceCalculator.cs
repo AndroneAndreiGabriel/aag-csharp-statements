177a0b9eb8d5fce1e6a60ae0c89544aaaa8e5b44
Andrei Androne <andrei.androne@outlook.com>
2022-02-09 21:16:03 +0200

    PriceCalculator

---
+++ PriceCalculator/PriceCalculator/PriceCalculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculator
+{
+    public static class PriceCalculator
+    {
+        
+        public static decimal CalculatePrice(
+                string name, 
+                decimal price, 
+                int quantity = 1, 
+                decimal vatPercent = 19)
+        {
+            return quantity * price * (100 + vatPercent / 100);
+        }
+    }
+}
---
+++ PriceCalculator/PriceCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal price1 = PriceCalculator.CalculatePrice(
+                name: "Paine", 
+                price: 3.5M);
+            decimal price2 = PriceCalculator.CalculatePrice(
+                name: "Apa", 
+                price: 2M, 
+                quantity: 5);
+             
+            decimal price3 = PriceCalculator.CalculatePrice(
+                name: "Someting else", 
+                price: 10.5M, 
+                quantity: 2, 
+                vatPercent: 21);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
